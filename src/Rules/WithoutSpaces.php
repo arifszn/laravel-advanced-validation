84e5a07d3b4b52e773045a1a38cddd068100b13f
Ariful Alam <swazan.arif@gmail.com>
2021-11-27 23:04:06 +0600

    Add rule WithoutSpaces

---
+++ src/Rules/WithoutSpaces.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Arifszn\AdvancedValidation\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * The field under validation must not contain spaces.
+ *
+ * @package Arifszn\AdvancedValidation\Rules
+ */
+class WithoutSpaces implements Rule
+{
+    
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return preg_match('/^\S*$/u', $value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must not contain spaces.';
+    }
+}
