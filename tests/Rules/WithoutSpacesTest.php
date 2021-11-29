53b1955064462630100bb7b84bb8bac47229a4a9
Ariful Alam <swazan.arif@gmail.com>
2021-11-29 23:22:02 +0600

    Add test for WithoutSpaces

---
+++ tests/Rules/WithoutSpacesTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Arifszn\AdvancedValidation\Tests\Rules;
+
+use Arifszn\AdvancedValidation\Rules\WithoutSpaces;
+use Arifszn\AdvancedValidation\Tests\TestCase;
+
+class WithoutSpacesTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function testAdd($result, $value)
+    {
+        $this->assertEquals($result, (new WithoutSpaces())->passes('foo', $value));
+    }
+
+    public function provider()
+    {
+        return [
+            [true, 'abc'],
+            [true, '123'],
+            [false, 'abc 123'],
+            [false, 'abc abc abc'],
+        ];
+    }
+}
