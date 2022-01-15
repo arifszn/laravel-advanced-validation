47df2b81a81e748986db4e9eb4eb7b25971152c4
Ariful Alam <swazan.arif@gmail.com>
2022-01-16 00:44:06 +0600

    Change error message of image_url

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -4,7 +4,7 @@
 return [
     'base64_image' => 'The :attribute is not valid Base64 encoded image.',
     'base64_string' => 'The :attribute is not valid Base64 encoded string.',
-    'image_url' => 'The :attribute is not valid image URL.',
+    'image_url' => 'The :attribute must be a valid image URL.',
     'phone' => 'The :attribute must be a valid phone number.',
     'without_spaces' => 'The :attribute must not contain spaces.',
 ];
