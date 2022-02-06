bcd70336ae3046d125f6c4d505aec466c3e94dd3
Ariful Alam <swazan.arif@gmail.com>
2022-02-06 23:39:04 +0600

    Test BIC rule

---
+++ tests/Rules/BICTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Arifszn\AdvancedValidation\Tests\Rules;
+
+use Arifszn\AdvancedValidation\Rules\BIC;
+use Arifszn\AdvancedValidation\Tests\TestCase;
+
+class BICTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function testValidation($result, $value)
+    {
+        $this->assertEquals($result, (new BIC())->passes('foo', $value));
+    }
+
+    public function provider()
+    {
+        return [
+            [true, 'SBICKEN1345'],
+            [true, 'SBICKEN1'],
+            [true, 'SBICKENY'],
+            [true, 'SBICKEN1YYP'],
+            [false, 'SBIC23NXXX'],
+            [false, 'S23CKENXXXX'],
+            [false, 'SBICKENXX'],
+            [false, 'SBICKENXX9'],
+            [false, 'SBICKEN13458'],
+            [false, 'SBICKEN'],
+        ];
+    }
+}
