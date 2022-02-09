db4323c9dec8e581e6bf46c649a330b82c28be88
Ariful Alam <swazan.arif@gmail.com>
2022-02-09 18:42:20 +0600

    Add rule DataURI

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -8,6 +8,7 @@ return [
     'bic' => 'The :attribute must be a BIC or SWIFT code.',
     'btc_address' => 'The :attribute must be a valid BTC address.',
     'credit_card' => 'The :attribute must be a valid credit card number.',
+    'data_uri' => 'The :attribute must have data uri format.',
     'image_url' => 'The :attribute must be a valid image URL.',
     'phone' => 'The :attribute must be a valid phone number.',
     'without_spaces' => 'The :attribute must not contain spaces.',
---
+++ src/Rules/DataURI.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Arifszn\AdvancedValidation\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * The field under validation must have data uri format.
+ *
+ * @package Arifszn\AdvancedValidation\Rules
+ */
+class DataURI implements Rule
+{
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @param string $message   Custom error message.
+     * @return void
+     */
+    public function __construct(string $message = null)
+    {
+        $this->message = $message ? $message : trans('advancedValidation::validation.data_uri');
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $validMediaType = '/^[a-z]+\/[a-z0-9\-\+]+$/i';
+
+        $validAttribute = '/^[a-z\-]+=[a-z0-9\-]+$/i';
+
+        $validData = '/^[a-z0-9!\$&\'\(\)\*\+,;=\-\._~:@\/\?%\s]*$/i';
+
+        $data = explode(",", $value);
+
+        if (count($data) < 2) {
+            return false;
+        }
+
+        $attributes = explode(";", trim(array_shift($data)));
+
+        $schemeAndMediaType = array_shift($attributes);
+
+        if (substr($schemeAndMediaType, 0, 5) !== 'data:') {
+            return false;
+        }
+
+        $mediaType = substr($schemeAndMediaType, 5);
+
+        if ($mediaType !== '' && !preg_match($validMediaType, $mediaType)) {
+            return false;
+        }
+
+        for ($i = 0; $i < count($attributes); $i++) {
+            if (
+                !($i === count($attributes) - 1 && strtolower($attributes[$i]) === 'base64') &&
+                !preg_match($validAttribute, $attributes[$i])
+            ) {
+                return false;
+            }
+        }
+
+        for ($i = 0; $i < count($data); $i++) {
+            if (!preg_match($validData, $data[$i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return $this->message;
+    }
+}
