59b3cfb154e001347a4e9256d2bb85a7b7928e9b
Ariful Alam <swazan.arif@gmail.com>
2022-03-02 22:41:11 +0600

    Remove white space

--- src/Rules/Hash.php
+++ src/Rules/Hash.php
@@ -5,8 +5,8 @@ namespace Arifszn\AdvancedValidation\Rules;
 use Illuminate\Contracts\Validation\Rule;
 
 /**
- * The field under validation must be a hash of type algorithm. Algorithm is one of 
- * ['md4', 'md5', 'sha1', 'sha256', 'sha384', 'sha512', 'ripemd128', 'ripemd160', 
+ * The field under validation must be a hash of type algorithm. Algorithm is one of
+ * ['md4', 'md5', 'sha1', 'sha256', 'sha384', 'sha512', 'ripemd128', 'ripemd160',
  * 'tiger128', 'tiger160', 'tiger192', 'crc32', 'crc32b']
  *
  * @package Arifszn\AdvancedValidation\Rules
